6b55b0516fd02c77ffc67dcbfeaacaf483f74752
xsence <756219922@qq.com>
2020-06-28 14:10:25 +0800

    6-28 release

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys, os
 import frame
 from PyQt5 import QtCore, QtGui, QtWidgets
 import PyWinMouse as mouse
+from system_hotkey import SystemHotkey
 
 cap_size = 64
 cap_zoom = 4
 
-
 def cap_screen():
     pos = ui.mouse.get_mouse_pos()
     if ui.cur_pos != pos:
-        print(pos)
+        #print(pos)
         if (pos[0] + 1) < cap_size/2:
             grab_x = 0
             pixel_x = pos[0]
@@ -43,12 +43,72 @@ def cap_screen():
         ui.line_h.setStyleSheet("color: "+QtGui.QColor(255-r, 255-g, 255-b).name()+";")
         ui.line_v.setStyleSheet("color: "+QtGui.QColor(255-r, 255-g, 255-b).name()+";")
 
+def cap_color():
+    #print(ui.xcolor.text())
+
+    ui.color_list.insertRow(0)
+    send.run(ui.xcolor.text())
+
+    #ui.color_list.setItem(0, 1, QtWidgets.QTableWidgetItem())
+    #ui.color_list.setItem(0, 0, QtWidgets.QTableWidgetItem())
+    
+    # ui.color_list.item(0,1).setText(ui.xcolor.text())
+    # back_color = QtGui.QColor()
+    # back_color.setNamedColor(ui.xcolor.text())
+    # brush = QtGui.QBrush(back_color)
+    # brush.setStyle(QtCore.Qt.SolidPattern)
+    # ui.color_list.item(0,0).setBackground(brush)
+    # ui.color_list.item(0,0).setFlags(QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+
+#https://github.com/timeyyy/system_hotkey
+#sig_hotkey = QtCore.pyqtSignal(str)
+#sig_hotkey.connect(cap_Color)
+sig_cap_color = SystemHotkey()
+sig_cap_color.register(("alt","s"), callback=lambda x: cap_color())
+
+#class receive send signal
+class x_signal(QtCore.QObject):
+    send_sig = QtCore.pyqtSignal(object)
+    def run(self,value):
+        self.send_sig.emit(value)
+
+#class response to signal
+class x_Ui_Dialog(frame.Ui_Dialog):
+    def x_setitem(self,value):
+        self.color_list.setItem(0,0,QtWidgets.QTableWidgetItem())
+        self.color_list.setItem(0,1,QtWidgets.QTableWidgetItem(value))
+        back_color = QtGui.QColor()
+        back_color.setNamedColor(value)
+        brush = QtGui.QBrush(back_color)
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.color_list.item(0,0).setBackground(brush)
+        self.color_list.item(0,0).setFlags(QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        self.color_list.setFocus()
+        self.color_list.setCurrentCell(0,1)
+    def clear_list(self):
+        self.color_list.clear()
+        self.color_list.setRowCount(0)
+    def copy_list(self):
+        clipboard = QtGui.QGuiApplication.clipboard()
+        if(self.color_list.rowCount() == 0):
+            cap_color()
+        clipboard.setText(self.color_list.currentItem().text())
+        self.color_list.setFocus()
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = frame.Ui_Dialog()
+    #ui = frame.Ui_Dialog()
+    ui=x_Ui_Dialog()
     ui.setupUi(MainWindow)
     MainWindow.show()
+
+    #send_sig and slot
+    send = x_signal()
+    send.send_sig.connect(ui.x_setitem)
+
+    ui.btn_clear.clicked.connect(ui.clear_list)
+    ui.btn_copy.clicked.connect(ui.copy_list)
 
     screen = QtWidgets.QApplication.primaryScreen()
     #window, something error now
@@ -56,7 +116,7 @@ if __name__ == "__main__":
     #img = screen.grabWindow(hwnd).toImage()
     ui.img_preview.setGeometry(QtCore.QRect(10, 10, 256, 256))
     ui.xcolor.setGeometry(QtCore.QRect(10, 276, 90, 30))
-    ui.pushButton.setGeometry(QtCore.QRect(295, 276, 90, 40))
+    ui.btn_clear.setGeometry(QtCore.QRect(274, 276, 90, 30))
     ui.line_h.setGeometry(QtCore.QRect(11, 135, 256, 1))
     ui.line_v.setGeometry(QtCore.QRect(135, 11, 1, 256))
     ui.color_preview.setGeometry(QtCore.QRect(131, 131, 9, 9))
